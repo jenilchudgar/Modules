9016acffabdc5053117e0feb3e4de5d2a0da8444
Jenil Chudgar <jenilchudgarfamily@gmail.com>
2023-05-19 22:17:12 +0530

    #

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup,find_packages
 
 setup(
-    name="your-module-name",
+    name="PySwitchCase",
     version="1.0.0",
-    author="Your Name",
-    author_email="your@email.com",
-    description="Description of your module",
-    long_description="",
+    author="JenilChudgar",
+    author_email="jenilchudgar@gmail.com",
+    description="The Switch In Python.",
+    long_description="The Switch Case construct is a programming structure that allows developers to execute different blocks of code based on the value of a given expression or variable. Although Python does not have a built-in switch statement like some other programming languages (e.g., C++ or Java), there are several ways to achieve similar functionality using alternative approaches.",
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/your-module",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
